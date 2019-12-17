f9b4dae20a3b1fba6baa0932bb05dbdc3fc08cbf
sethvincent <sethvincent@gmail.com>
2019-12-16 17:11:03 -0800

    add cors headers

--- mbview.js
+++ mbview.js
@@ -12,6 +12,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 module.exports = {
 
   /**
